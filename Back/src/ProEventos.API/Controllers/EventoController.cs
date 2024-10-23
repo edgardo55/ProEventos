687a93b489a45d30cb90ff58870061f785ddf5eb
Edgardo Samaji <edgardosamajii@gmail.com>
2024-10-23 07:45:18 +0100

    secao 2: Dotnet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public Eventos Get()
+        {
+            return new Evento() {
+                EventoId = 1,
+                Local = "Belo Horizonte",
+                Lote = "1 Lote",
+                QtdPessoas = 250,
+                DataEvento = DataTime.Now.AddDays(2).ToString
+            };
+        }
+    }
+}
